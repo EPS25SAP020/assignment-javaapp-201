b27aec404abd95353a6901b2c32ac28aabb26115
Gude Sailaja <sailajagude77@gmail.com>
2025-08-21 15:27:11 +0530

    Short description of work done.

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+package com.cognizant.javaapp201.main;
+
+import com.cognizant.javaapp201.operation.SailajaDivision;
+
+public class App {
+	public static void main(String[] args) {
+		SailajaDivision sailajadivision = new SailajaDivision();
+		int result = sailajadivision.divide(10,2);
+	}
+}
---
+++ src/main/java/SailajaDivision.java
@@ -0,0 +1,29 @@
+package com.cognizant.javaapp201.operation;
+
+import com.cognizant.javaapp201.mathematics.Division;
+
+public class SailajaDivision extends Division {
+	public int divide(byte num1, byte num2) {
+		return num1/num2;
+	}
+	
+	public int divide(short num1, short num2) {
+		return num1/num2;
+	}
+	
+	public int divide(int num1, int num2) {
+		return num1/num2;
+	}
+	
+	public long divide(long num1, long num2) {
+		return num1/num2;
+	}
+	
+	public double divide(float num1, float num2) {
+		return num1/num2;
+	}
+	
+	public double divide(double num1, double num2) {
+		return num1/num2;
+	}
+}
