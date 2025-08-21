d2dc1d19c52a9e7152975684c14a5496e9536349
Mirza Farhana Sulthana <mirzafarhana1824@gmail.com>
2025-08-21 15:25:10 +0530

    Added the code for functionality.

---
+++ src/main/java/Program.java
@@ -0,0 +1,9 @@
+package com.cognizant.javaapp201.main;
+import com.cognizant.javaapp201.operation.FarhanaDivision;
+public class Program{
+	public static void main(String[] args){
+		FarhanaDivision farhanaDivision = new FarhanaDivision();
+		
+		int result = farhanaDivision.divide(20,4);
+	}
+}
