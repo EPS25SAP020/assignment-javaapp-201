696b458a07468c2afc36631e3b5269522f63f770
Krishnendhu R <krishnendhur84@gmail.com>
2025-08-21 16:04:19 +0530

    Added the code for Functionality

---
+++ src/main/java/KrishnendhuSubtraction.java
@@ -0,0 +1,49 @@
+package com.cognizant.javaapp201.operation;
+import com.cognizant.javaapp201.mathematics.Subtraction;
+
+
+public class KrishnendhuSubtraction extends Subtraction {
+	
+	public int subtract(byte num1, byte num2) {
+		
+		int result=num1-num2;
+        return result;
+		
+	}
+		public int subtract(short num1, short num2) {		
+		int result=num1-num2;
+        return result;
+		
+	}
+	
+	public int subtract(int num1, int num2) {
+		
+		int result=num1-num2;
+        return result;
+		
+	}
+	
+	
+	public long subtract(long num1,long num2) {
+
+			 long result=num1-num2;
+			 return result;
+
+		}
+        public double subtract(float num1, float num2) {		
+		double result=num1-num2;
+        return result;
+		
+	}
+	public double subtract(double num1, double num2) {
+		
+		double result=num1-num2;
+        return result;
+		
+	}
+	
+ 
+ 
+ 
+ 
+ }
--- src/main/java/com/cognizant/App.java
+++ src/main/java/com/cognizant/App.java
@@ -1,7 +1,37 @@
 package com.cognizant;
+import  com.cognizant.javaapp201.operation.KrishnendhuSubtraction;
+import com.cognizant.javaapp201.mathematics.Subtraction;
+
 
 public class App {
     public static void main(String[] args) {
+		
+		KrishnendhuSubtraction krishnendhuSubtraction=new KrishnendhuSubtraction();
+		int bytesubresult=krishnendhuSubtraction.subtract((byte)10,(byte)2);
+				System.out.println("Subtraction=" +bytesubresult);
+
+		
+		int shortsubresult=krishnendhuSubtraction.subtract((short)10,(short)2);
+				System.out.println("Subtraction=" +shortsubresult);
+
+		
+		int intsubresult=krishnendhuSubtraction.subtract(10,2);
+				System.out.println("Subtraction=" +intsubresult);
+
+		
+		
+		long longsubresult=krishnendhuSubtraction.subtract(10L,2L);
+		System.out.println("Subtraction=" +longsubresult);
+		
+		
+		double floatsubresult=krishnendhuSubtraction.subtract(10.2f,2.2f);
+				System.out.println("Subtraction=" +floatsubresult);
+
+		
+		
+		double doublesubresult=krishnendhuSubtraction.subtract(10.33d,2.23d);
+				System.out.println("Subtraction=" +doublesubresult);
+
 		
     }
 }
