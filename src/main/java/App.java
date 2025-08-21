a6815caeb005a8bb26089669782b01e4ab2aa044
Mavikka S <mavikkas@gmail.com>
2025-08-21 15:19:08 +0530

    added code for assignment

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+package com.cognizant.javaapp201.main;
+
+import com.cognizant.javaapp201.operation.MavikkaDivision;
+
+public class App {
+    public static void main(String[] args) {	
+		MavikkaDivision mavikkaDivision = new MavikkaDivision();
+		
+		int result = mavikkaDivision.divide(10,2);		
+		
+    }
+}
