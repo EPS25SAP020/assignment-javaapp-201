24c443b14c7479126ec6b0a9264f89bfad2c982a
Buttaru Priyanka <priyankabuttaru@gmail.com>
2025-08-21 17:59:10 +0530

    added the code for functionality.

---
+++ src/main/java/Program.java
@@ -0,0 +1,9 @@
+package com.cognizant.javaapp201.main;
+import com.cognizant.javaapp201.operation.PriyankaDivision;
+public class Program{
+    public static void main(String[] args){
+        PriyankaDivision priyankaDivision = new PriyankaDivision();
+       
+        int result = priyankaDivision.divide(20,4);
+    }
+}
