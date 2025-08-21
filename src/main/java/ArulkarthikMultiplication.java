88a35c842c746e866b7668966f03621d666da404
Arulkarthik S <arulkarthik2511@gmail.com>
2025-08-21 15:27:15 +0530

    inheritance done ><.

---
+++ src/main/java/ArulkarthikMultiplication.java
@@ -0,0 +1,29 @@
+package com.cognizant.javaapp01.operation;
+
+import com.cognizant.javaapp201.mathematics.Multiplication;
+
+public class ArulkarthikMultiplication extends Multiplication {
+	public int multiply(byte num1, byte num2) {
+		return num1 * num2;
+	}
+	
+	public int multiply(short num1, short num2) {
+		return num1 * num2;
+	}
+	
+	public int multiply(int num1, int num2) {
+		return num1 * num2;
+	}
+	
+	public long multiply(long num1, long num2) {
+		return num1 * num2;
+	}
+	
+	public double multiply(float num1, float num2) {
+		return num1 * num2;
+	}
+	
+	public double multiply(double num1, double num2) {
+		return num1 * num2;
+	}
+}
--- src/main/java/com/cognizant/App.java
+++ src/main/java/com/cognizant/App.java
@@ -1,7 +1,25 @@
 package com.cognizant;
 
+import com.cognizant.javaapp01.operation.ArulkarthikMultiplication;
+
 public class App {
     public static void main(String[] args) {
+		ArulkarthikMultiplication arulkarthikMultiplication = new ArulkarthikMultiplication();
+		
+		int val1 = arulkarthikMultiplication.multiply((byte)1,(byte)1);
+		System.out.println(val1);
+		int val2 = arulkarthikMultiplication.multiply((short)1,(short)1);
+		System.out.println(val2);
+		int val3 = arulkarthikMultiplication.multiply(1,1);
+		System.out.println(val3);
+		long val4 = arulkarthikMultiplication.multiply(100000,1);
+		System.out.println(val4);
+		double val5 = arulkarthikMultiplication.multiply(1F,40F);
+		System.out.println(val5);
+		double val6 = arulkarthikMultiplication.multiply(111.22,1.34);
+		System.out.println(val6);
+		
+		
 		
     }
 }
