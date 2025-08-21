44a2ea456f3d205168ccb9b99d38246bb7d4042d
Kuppi Reddy Teja Sree <tejachittu2003@gmail.com>
2025-08-21 15:18:55 +0530

    Added to the code for functionality

--- src/main/java/com/cognizant/App.java
+++ src/main/java/com/cognizant/App.java
@@ -1,7 +1,26 @@
 package com.cognizant;
 
+import com.cognizant.javaapp201.operation.TejasreeSubraction;
+
 public class App {
     public static void main(String[] args) {
+		TejasreeSubraction tejasreeSubraction = new TejasreeSubraction();
+		int subByte = tejasreeSubraction.subtract((byte)10, (byte)2);
+		System.out.println(subByte);
 		
+		int subShort = tejasreeSubraction.subtract((short)10, (short)2);
+		System.out.println(subShort);
+		
+		int subInt = tejasreeSubraction.subtract(10, 2);
+		System.out.println(subInt);
+		
+		long subLong = tejasreeSubraction.subtract(10L, 2L);
+		System.out.println(subLong);
+		
+		double subFloat = tejasreeSubraction.subtract(10f, 2f);
+		System.out.println(subFloat);
+		
+		double subDouble = tejasreeSubraction.subtract(10, 2);
+		System.out.println(subDouble);
     }
 }
