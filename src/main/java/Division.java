fa1da54d9476a5b776943219b60129b802fc8ceb
Sinchana U Hegde <lakkisinchana@gmail.com>
2025-08-21 16:39:32 +0530

    add the code fumctionally

--- src/main/java/Division.java
+++ src/main/java/Division.java
@@ -1,26 +1,22 @@
 package com.cognizant.javaapp201.mathematics;
-
+ 
 public class Division {
+ 
 	public int divide(byte num1, byte num2) {
 		return 0;
 	}
-	
 	public int divide(short num1, short num2) {
 		return 0;
 	}
-	
 	public int divide(int num1, int num2) {
 		return 0;
 	}
-	
 	public long divide(long num1, long num2) {
 		return 0;
 	}
-	
 	public double divide(float num1, float num2) {
 		return 0;
 	}
-	
 	public double divide(double num1, double num2) {
 		return 0;
 	}
---
+++ src/main/java/SinchanauhegdeDivision.java
@@ -0,0 +1,26 @@
+
+package com.cognizant.javaapp201.operation;
+ 
+import com.cognizant.javaapp201.mathematics.Division; 
+public class SinchanauhegdeDivision extends Division{
+    public int divide(byte num1, byte num2) {
+		return num1/num2;
+	}
+	public int divide(short num1, short num2) {
+		return num1/num2;
+	}
+	public int divide(int num1, int num2) {
+		return num1/num2;
+	}
+	public long divide(long num1, long num2) {
+		return num1/num2;
+	}
+	public double divide(float num1, float num2) {
+		return num1/num2;
+	}
+	public double divide(double num1, double num2) {
+		return num1/num2;
+	}
+}
+ 
+ 
