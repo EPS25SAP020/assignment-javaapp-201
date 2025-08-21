f15add24c913451c1516600230f5ddd50049d210
Arvind Kumar <arvind2341989@gmail.com>
2025-08-21 15:41:40 +0530

    Added solution for performing the task of addition.

--- src/main/java/com/cognizant/App.java
+++ src/main/java/com/cognizant/App.java
@@ -1,7 +1,16 @@
 package com.cognizant;
 
+import com.cognizant.javaapp201.operation.ArvindAddition;
+
 public class App {
     public static void main(String[] args) {
+		ArvindAddition arvindAddition = new ArvindAddition();
 		
+		System.out.println(arvindAddition.add((byte)10, (byte)2));
+		System.out.println(arvindAddition.add((short)10, (short)2));
+		System.out.println(arvindAddition.add(10, 2));
+		System.out.println(arvindAddition.add(10L, 2L));
+		System.out.println(arvindAddition.add(10.12F, 2.2F));
+		System.out.println(arvindAddition.add(10.12, 2.2));
     }
 }
