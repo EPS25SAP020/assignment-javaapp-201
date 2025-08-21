0c3aa49e480a84a8fdec447300bc6a1d7c192683
Diana Liz Kuriakose <dianalizkuriakose@gmail.com>
2025-08-21 15:22:50 +0530

    Implemented addition functionality

---
+++ src/main/java/DianaAddition.java
@@ -0,0 +1,41 @@
+package com.cognizant.javaapp201.operation;
+
+import com.cognizant.javaapp201.mathematics.Addition;
+
+public class DianaAddition extends Addition {
+	public int add(byte num1, byte num2) {
+		int result = num1 + num2;
+		
+		return result;
+	}
+	
+	public int add(short num1, short num2) {
+		int result = num1 + num2;
+		
+		return result;
+	}
+	
+	public int add(int num1, int num2) {
+		int result = num1 + num2;
+		
+		return result;
+	}
+	
+	public long add(long num1, long num2) {
+		long result = num1 + num2;
+		
+		return result;
+	}
+	
+	public double add(float num1, float num2) {
+		double result = num1 + num2;
+		
+		return result;
+	}
+	
+	public double add(double num1, double num2) {
+		double result = num1 + num2;
+		
+		return result;
+	}
+}
--- src/main/java/com/cognizant/App.java
+++ src/main/java/com/cognizant/App.java
@@ -1,7 +1,16 @@
 package com.cognizant;
 
+import com.cognizant.javaapp201.operation.DianaAddition;
+
 public class App {
     public static void main(String[] args) {
+		DianaAddition dianaAddition = new DianaAddition();
 		
+		System.out.println( "Bytes Addition(10,2):" + dianaAddition.add( (byte)10, (byte)2 ) );
+		System.out.println( "Shorts Addition(10,2):" + dianaAddition.add( (short)10, (short)2 ) );
+		System.out.println( "Int Addition(10,2):" + dianaAddition.add( 10, 2 ) );
+		System.out.println( "Float Addition(10.01,2.03):" + dianaAddition.add( 10.01F, 2.03F ) );
+		System.out.println( "Long Addition(10,2):" + dianaAddition.add( 10L, 2L ) );
+		System.out.println( "Double Addition(10.01,2.03):" + dianaAddition.add( 10.01, 2.03 ) );
     }
 }
